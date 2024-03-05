23d46b4507e38c77c9b70d98fbd298aa89c6616c
paulaasoriano2 <132095954+paulaasoriano2@users.noreply.github.com>
2024-03-05 20:59:06 +0100

    Modificada la pantalla de google

--- aversifunciona/lib/google.dart
+++ aversifunciona/lib/google.dart
@@ -1,3 +1,4 @@
+import 'package:aversifunciona/pantalla_principal.dart';
 import 'package:flutter/material.dart';
 
 class Google extends StatelessWidget {
@@ -5,10 +6,86 @@ class Google extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google'),
+        title: Text('Inicia sesión con Google'),
       ),
-      body: Center(
-        child: Text('Pantalla de Google'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                'Selecciona una cuenta',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            SizedBox(height: 8), // Adjust as needed
+            Center(
+              child: Row(
+                children: [
+                  Text(
+                    'para ir a ',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  Text(
+                    'Musify',
+                    style: TextStyle(fontSize: 14, color: Colors.blue),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            AccountTile(
+              accountName: '819684@unizar.es',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => pantalla_principal()),
+                );
+              },
+            ),
+            AccountTile(
+              accountName: 'helalizineb@gmail.com',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => pantalla_principal()),
+                );
+              },
+            ),
+            // Puedes agregar más cuentas según sea necesario
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AccountTile extends StatelessWidget {
+  final String accountName;
+  final VoidCallback onPressed;
+
+  const AccountTile({
+    required this.accountName,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Row(
+          children: [
+            Icon(Icons.account_circle, size: 40),
+            SizedBox(width: 16),
+            Text(
+              accountName,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
       ),
     );
   }
