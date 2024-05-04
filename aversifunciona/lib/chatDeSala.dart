1880bad1b6c72eb5b154ced9e9a81d01b8c6336d
paulaasoriano2 <132095954+paulaasoriano2@users.noreply.github.com>
2024-05-04 10:23:12 +0200

    Avances pantalla chat de sala

--- aversifunciona/lib/chatDeSala.dart
+++ aversifunciona/lib/chatDeSala.dart
@@ -1,6 +1,9 @@
+import 'package:aversifunciona/biblioteca.dart';
+import 'package:aversifunciona/buscar.dart';
+import 'package:aversifunciona/pantalla_principal.dart';
+import 'package:aversifunciona/salas.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 
 class ChatDeSala extends StatefulWidget {
   final String roomName;
@@ -23,7 +26,7 @@ class _ChatDeSalaState extends State<ChatDeSala> {
   Future<void> _loadMessages() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
-      _messages = prefs.getStringList('chat_messages_$widget.roomName') ?? [];
+      _messages = prefs.getStringList('chat_messages_${widget.roomName}') ?? [];
     });
   }
 
@@ -41,7 +44,7 @@ class _ChatDeSalaState extends State<ChatDeSala> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.black,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {
@@ -61,85 +64,200 @@ class _ChatDeSalaState extends State<ChatDeSala> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              child: ListView.builder(
-                reverse: true,
-                itemCount: _messages.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(10),
+      body: Container(
+        color: Color(0xFF333333), // Gris oscuro
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                child: ListView.builder(
+                  reverse: true,
+                  itemCount: _messages.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Text(
+                            _messages[index],
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.black, // Fondo negro para la barra de opciones inferior
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white, // Fondo blanco para el recuadro de texto del mensaje
+                              borderRadius: BorderRadius.circular(20), // Bordes redondeados
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: TextField(
+                                controller: _messageController,
+                                style: TextStyle(color: Colors.black), // Texto negro para mejor legibilidad
+                                decoration: InputDecoration(
+                                  hintText: 'Escribe un mensaje...',
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none, // Sin borde para que el BoxDecoration defina el borde
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          String message = _messageController.text;
+                          _saveMessage(message);
+                          _messageController.clear();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
                         ),
                         child: Text(
-                          _messages[index],
+                          'Enviar',
                           style: TextStyle(color: Colors.white),
                         ),
                       ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(width: 1.0, color: Colors.white),
-              ),
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      controller: _messageController,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        hintText: 'Escribe un mensaje...',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    height: 70,
+                    decoration: BoxDecoration(
+                      border: Border(
+                        top: BorderSide(width: 1.0, color: Colors.white),
                       ),
                     ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    String message = _messageController.text;
-                    _saveMessage(message);
-                    _messageController.clear();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => pantalla_principal()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 8),
+                              Icon(Icons.house_outlined, color: Colors.grey, size: 37.0),
+                              Text(
+                                'Inicio',
+                                style: TextStyle(color: Colors.white, fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => pantalla_buscar()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 8),
+                              Icon(Icons.question_mark_outlined, color: Colors.grey, size: 37.0),
+                              Text(
+                                'Buscar',
+                                style: TextStyle(color: Colors.white, fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => pantalla_biblioteca()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 8),
+                              Icon(Icons.library_books_rounded, color: Colors.grey, size: 37.0),
+                              Text(
+                                'Biblioteca',
+                                style: TextStyle(color: Colors.white, fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => pantalla_salas()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 8),
+                              Icon(Icons.chat_bubble_rounded, color: Colors.grey, size: 37.0),
+                              Text(
+                                'Salas',
+                                style: TextStyle(color: Colors.white, fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  child: Text(
-                    'Enviar',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
